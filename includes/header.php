a98fbfb02243d493521cbb3998b39079eb27c156
oyinkansola <yahayaoyinkansola@gmail.com>
2023-07-13 03:44:23 +0100

    Added header.php file

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<?php require_once "../config/config.php" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="<?php echo WEB_URL ?>img/favicon.png" />
+    <link rel="stylesheet" href="<?php echo WEB_URL ?>css/bootstrap.min.css" />
+    <link rel="stylesheet" href="<?php echo WEB_URL ?>css/lnr-icon.css" />
+    <link rel="stylesheet" href="<?php echo WEB_URL ?>css/font-awesome.min.css" />
+    <link rel="stylesheet" href="<?php echo WEB_URL ?>css/style.css" />
+    <title><?php echo $title ?? "Login" ?></title>
+  </head>
+
+  <body>
