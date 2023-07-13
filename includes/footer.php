aee44bac7eb97e61df597f7387d1398923c43614
oyinkansola <yahayaoyinkansola@gmail.com>
2023-07-13 03:44:40 +0100

    Added the footer.php file

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+    <script src="<?php echo WEB_URL ?>js/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo WEB_URL ?>js/popper.min.js"></script>
+    <script src="<?php echo WEB_URL ?>js/bootstrap.min.js"></script>
+    <script src="<?php echo WEB_URL ?>plugins/theia-sticky-sidebar/ResizeSensor.js"></script>
+    <script src="<?php echo WEB_URL ?>plugins/theia-sticky-sidebar/theia-sticky-sidebar.js"></script>
+    <script src="<?php echo WEB_URL ?>js/script.js"></script>
+  </body>
+</html>
