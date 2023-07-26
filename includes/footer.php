f014ee2c5c4ab60ad9608d21e4733f07cb359385
oyinkansola <yahayaoyinkansola@gmail.com>
2023-07-26 06:46:39 +0100

    Include select2 vendor libraries

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,8 @@
     <script src="<?php echo WEB_URL ?>js/bootstrap.min.js"></script>
     <script src="<?php echo WEB_URL ?>plugins/theia-sticky-sidebar/ResizeSensor.js"></script>
     <script src="<?php echo WEB_URL ?>plugins/theia-sticky-sidebar/theia-sticky-sidebar.js"></script>
+    <script src="<?php echo WEB_URL ?>plugins/select2/moment.min.js"></script>
+    <script src="<?php echo WEB_URL ?>plugins/select2/select2.min.js"></script>
     <script src="<?php echo WEB_URL ?>js/script.js"></script>
   </body>
 </html>
