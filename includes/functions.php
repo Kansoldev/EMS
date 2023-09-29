13a771a428228b96b8e325451da8414b16b53861
oyinkansola <yahayaoyinkansola@gmail.com>
2023-09-29 04:25:08 +0100

    Added dateDiff function For finding the difference between 2 dates

--- includes/functions.php
+++ includes/functions.php
@@ -21,3 +21,10 @@
         $stmt->execute();
         return $stmt->fetchAll();
     }
+
+    function dateDiff($start, $end) {
+		$start_ts = strtotime($start);
+		$end_ts = strtotime($end);
+		$diff = $end_ts - $start_ts;
+		return round($diff / 86400);
+	}
